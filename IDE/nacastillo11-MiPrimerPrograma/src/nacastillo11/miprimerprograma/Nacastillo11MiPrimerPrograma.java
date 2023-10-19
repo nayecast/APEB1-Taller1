2308a9ac41b7e06e3c339ed2db421bb7dff55417
nayecast <nacastillo11@utpl.edu.ec>
2023-10-19 17:16:11 -0500

    Este es mi primer programa que subo

---
+++ IDE/nacastillo11-MiPrimerPrograma/src/nacastillo11/miprimerprograma/Nacastillo11MiPrimerPrograma.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nacastillo11.miprimerprograma;
+
+/**
+ *
+ * @author SALAI
+ */
+public class Nacastillo11MiPrimerPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+            
+      
+        // TODO code application logic here
+    }
+    
+}
